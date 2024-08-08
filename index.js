6de72f436c7068e9752c246c6fa302951efe6e82
FraPis23 <piscopofrancesco23@gmail.com>
2024-08-08 11:27:12 +0200

    work in progress

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import {PORT, mongoDBURL, mongoDBURL1, mongoDBURL2} from "./config.js";
 import mongoose from "mongoose";
 import cors from "cors";
+import {auth} from 'express-oauth2-jwt-bearer'
+
+import {PORT, mongoDBURL, mongoDBURL1, mongoDBURL2, HOST} from "./config.js";
+
 import userRoute from "./routes/userRoute.js";
 import warehouseRoute from "./routes/warehouseRoute.js";
 import thingRoute from "./routes/thingRoute.js";
-
 
 const app = express();
 
@@ -14,27 +16,39 @@ app.use(cors({
     optionSuccessStatus: 200
 })
 );
+
+const checkJwt = auth({
+    audience: 'http://localhost:5555/api/auth0',
+    issuerBaseURL: 'http://dev-8xr6cftsyhw0k2uw.us.auth0.com/',
+});
+
 app.use(express.json());
 
-app.use('/api/users', userRoute);
+app.use('/api/users', checkJwt, userRoute);
 
-app.use('/api/warehouses', warehouseRoute);
+app.use('/api/warehouses', checkJwt, warehouseRoute);
 
-app.use('/api/things', thingRoute)
+app.use('/api/things', checkJwt, thingRoute)
 
 app.get("/", (request, response) => {
     console.log(request);
     return response.status(234).send("Backend");
 });
 
-mongoose
-    .connect(mongoDBURL1)
-    .then(() => {
+const startServer = async () => {
+    try {
+        await mongoose.connect(mongoDBURL1,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            });
         console.log("MongoDB Connesso");
-        app.listen(PORT, () => {
-            console.log(`App in ascolto alla porta: ${PORT}`);
-        })
-    })
-    .catch((error) => {
-        console.log(error);
-    })
+        app.listen(PORT, HOST, () => {
+            console.log(`App in ascolto all'indirizzo http://${HOST}:${PORT}`);
+        });
+    } catch (error) {
+        console.error("Errore durante la connessione a MongoDB:", error);
+    }
+};
+
+startServer();
